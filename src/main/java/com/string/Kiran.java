924112d8a9bdff7f2edd0c3b61441b5dd94bf491
kirankumarkarmala1996 <108362315+kirankumarkarmala1996@users.noreply.github.com>
2023-03-24 19:05:59 +0530

    modification done

--- src/main/java/com/string/Kiran.java
+++ src/main/java/com/string/Kiran.java
@@ -1,6 +1,15 @@
 package com.string;
 
 public class Kiran {
+	// default constructor
+	public Kiran() {
+		
+	}
+	
+	// methods
+	public void methods() {
+		System.out.println("methods");
+	}
 public static void main(String[] args) {
 	System.out.println("kiran");
 }
